067e867b7b861c749e2b034aaba992bc41988bba
robert.wadhams <robert.wadhams@gmail.com>
2023-04-03 15:27:22 +1000

    Added FinancialDTO and ValidationService

---
+++ FinancialDBCommon/src/main/groovy/com/wadhams/financials/db/common/dto/FinancialDTO.groovy
@@ -0,0 +1,26 @@
+package com.wadhams.financials.db.common.dto
+
+import com.wadhams.financials.db.common.type.Asset
+import com.wadhams.financials.db.common.type.Category
+import com.wadhams.financials.db.common.type.ReportGrouping
+import com.wadhams.financials.db.common.type.SubCategory
+
+import groovy.transform.ToString
+import java.time.LocalDate
+
+@ToString(includeNames=true)
+class FinancialDTO {
+	LocalDate transactionDate
+	BigDecimal amount
+	String payee
+	String description
+	
+	Asset asset
+	Category category
+	SubCategory subCategory
+	LocalDate startDate
+	LocalDate endDate
+	ReportGrouping rg1
+	ReportGrouping rg2
+	ReportGrouping rg3
+}

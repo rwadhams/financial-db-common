7cb27fba1ebf6b3e9cf8002c3d1af4d8c90969d9
robert.wadhams <robert.wadhams@gmail.com>
2023-04-03 15:23:05 +1000

    Initial commit

---
+++ FinancialDBCommon/readme.txt
@@ -0,0 +1,8 @@
+README
+======
+
+Common artifacts used across other Financial-DB projects
+
+Gradle
+------
+clean build publishToMavenLocal
---
+++ FinancialDBCommon/src/main/groovy/com/wadhams/financials/db/common/type/SubCategory.groovy
@@ -0,0 +1,44 @@
+package com.wadhams.financials.db.common.type
+
+enum SubCategory {
+	Invalid('Invalid','Invalid'),
+	Unknown('Unknown','Unknown');
+	
+	private static EnumSet<SubCategory> allEnums = EnumSet.allOf(SubCategory.class)
+	
+	private final String name
+	private final String dbValue
+	
+	SubCategory(String name, String dbValue) {
+		this.name = name
+		this.dbValue = dbValue
+	}
+	
+	public static SubCategory findByName(String text) {
+		if (text) {
+			text = text.toUpperCase()
+			for (SubCategory e : allEnums) {
+				if (e.name.equals(text)) {
+					return e
+				}
+			}
+		}
+		else {
+			return SubCategory.Unknown
+		}
+		
+		println ''
+		println "ZZZZ Invalid sub-category text: $text"
+		println ''
+		return SubCategory.Invalid
+	}
+
+	public String getName() {
+		return name
+	}
+
+	public String getDbValue() {
+		return dbValue
+	}
+
+}

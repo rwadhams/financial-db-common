e2c7ea8683939e034c2b631f64e0165c2585b663
robert.wadhams <robert.wadhams@gmail.com>
2023-06-11 10:48:04 +1000

    Removed FraserIsland ReportGrouping

--- FinancialDBCommon/src/main/groovy/com/wadhams/financials/db/common/type/ReportGrouping.groovy
+++ FinancialDBCommon/src/main/groovy/com/wadhams/financials/db/common/type/ReportGrouping.groovy
@@ -1,10 +1,8 @@
 package com.wadhams.financials.db.common.type
 
 enum ReportGrouping {
-	SpecificRunningCost('SPECIFIC_RUNNING_COST','SPECIFIC_RUNNING_COST'),	//specific
+	SpecificRunningCost('SPECIFIC_RUNNING_COST','SPECIFIC_RUNNING_COST'),	//specific with start and end dates
 	OngoingRunningCost('ONGOING_RUNNING_COST','ONGOING_RUNNING_COST'),		//start and end dates are consecutive and don't overlap
-	
-	FraserIsland('FRASER_ISLAND','FRASER_ISLAND'),		//grouping for location-specific expenses
 	
 	CampHillReno('CAMP_HILL_RENO','CAMP_HILL_RENO'),	//grouping for specific expenses
 	
